cb15bdbdac5a1001a542c0753b7ba8e28d772003
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-02-19 15:15:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,5 +11,43 @@ client.on('ready', () => {
 	client.user.setUsername('ZIK!');
 });
 
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (message.channel.type === 'dm') return;
+
+	let prefix = config.prefix;
+	let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+
+	if(cmd == `${prefix}play`) {
+		    // Vérification 
+		if(!message.member.voiceChannel) 
+			return message.channel.send("Connectez-vous à un salon vocal !");
+		if(message.guild.me.voiceChannel) 
+			return message.channel.send("Le bot est déjà connecté à un salon vocal");
+		if(!args[0])
+			return message.channel.send("Merci de préciser un lien YouTube !");
+	
+		const validate = await ytdl.validateURL(args[0]);
+		if (!validate) return message.channel.send("Désolé, l'url n'est pas valide !");
+
+		if(message.member.voiceChannel) {
+			if(!message.guild.me.voiceChannel){
+				if(args[0]){
+					const info = ytdl.getInfo(args[0]);
+					const connection = message.member.voiceChannel.join();
+					const dispatcher = connection.playStream(ytdl(args[0], { filter: 'audioonly'}));
+					ytdl.getInfo(args[0]);
+
+					//message.member.voiceChannel.join();
+
+				}
+			}
+		}
+	}
+});
+
 // Login
 client.login(process.env.TOKEN);
