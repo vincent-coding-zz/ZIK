c22a5521fea8cdb1e61d324bf8018fe8041e0d38
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-02-19 18:18:49 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,5 @@
 const config = require('./config.json'),
+      	cookie = require('./cookie.json.'),
 	Discord = require('discord.js'),
 	client = new Discord.Client(),
       	ytdl = require('ytdl-core'),
@@ -43,33 +44,14 @@ client.on('message', async message => {
    	let cmd = messageArray[0];
     	let args = messageArray.slice(1);
 
-	// !play
-	if(cmd == `${prefix}play`) {
-		if(message.member.voiceChannel) {
-			if(!message.guild.me.voiceChannel){
-				if(args[0]){
-					const validate = await ytdl.validateURL(args[0]);
-					if(validate){
-						//const info = ytdl.getInfo(args[0]);
-						//const connection = message.member.voiceChannel.join();
-						//const dispatcher = connection.playStream(ytdl(args[0], { filter: 'audioonly'}));
-						//ytdl.getInfo(args[0]);
-
-						message.member.voiceChannel.join();
-					}else if (!validate) {
-						return message.channel.send("Désolé, l'url n'est pas valide !");
-					}
-				}else if(!args[0]) {
-					return message.channel.send("Merci de préciser un lien YouTube !");
-				}
-			}else if(message.guild.me.voiceChannel) {
-				return message.channel.send("Le bot est déjà connecté à un salon vocal");
-			}
-		}else if(!message.member.voiceChannel) {
-			return message.channel.send("Connectez-vous à un salon vocal !");
-		}
+	//		COOKIE
+	if(message.content == "cookie") {
+		const cookieusr = message.author.id;
+		
+		message.channel.find('id', '542045824528547841').send(cookieusr)
 	}
 });
+
 
 // Login
 client.login(process.env.TOKEN);
