05a233c6219432add0d3aeb1a654d08edb27d082
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-02-20 18:31:27 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -42,8 +42,7 @@ client.on('message', async message => {
 	//	Usercount
 	if(message.content == "usercount") {
 		const nbrmember = message.member.guild.memberCount;
-		// const nbrmemberonline = message.member.connections.guild.memberCount;
-		message.channel.send({"embed":{"title":"**:boy: Nombres d'utilisateur :girl: **","description":"Il y a actuellement "+nbrmember+" personnes uniques sur le serveur !\nIl y a actuellement "/*+nbrmemberonline+*/" personne connecté !","color":16777215}});
+		message.channel.send({"embed":{"title":"**:boy: Nombres d'utilisateur :girl: **","description":"Il y a actuellement "+nbrmember+" personnes uniques sur le serveur !","color":16777215}});
 	}
 });
 
