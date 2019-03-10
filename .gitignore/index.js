12aba58cf3a3255848f120f86a9004af21638327
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-03-10 20:29:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -257,8 +257,7 @@ client.on('message', msg => {
 	
 	// Profil
 	if(command === "profil") {		
-		const profilname = msg.member.user,
-		    profilid = "INSERTID";
+		const profilname = msg.member.user;
 		msg.channel.send({"embed": {
 			"color": 16777215,
 			"title": ":girl: Profil de :boy:" + profilname,
@@ -270,7 +269,7 @@ client.on('message', msg => {
 			    },
 			    {
 				"name": ":id:",
-				"value": profilid,
+				"value": "cc",
 				"inline": true
 			    }
 			]
